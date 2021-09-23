0a9eb660a7352a7e8f45d1691da69f13e725e18d
nikkijwang <nikkijwang@gmail.com>
2021-09-22 23:16:27 -0700

    remove start and end matchers and add more values

--- src/com/dollarsbank/utility/StringUtil.java
+++ src/com/dollarsbank/utility/StringUtil.java
@@ -1,10 +1,14 @@
 package com.dollarsbank.utility;
 
 public enum StringUtil {
-    NAME("^[a-zA-Z]+$"),
-    USERNAME("^[a-zA-Z]+[a-zA-Z0-9]+$"), 
-    PASSWORD("^(?=.*[a-z])(?=.*[A-Z])(?=.*[-+_!@#$%^&*.,?])[A-Za-z\\d@$!%*?&]{8,}$"),
-    CONFIRM("y(es)?\\Z|no?\\Z");
+    NAME("[a-zA-Z]+"),
+    USERNAME("[a-zA-Z]+[a-zA-Z0-9]+"), 
+    PASSWORD("(?=.*[a-z])(?=.*[A-Z])(?=.*[-+_!@#$%^&*.,?])[A-Za-z\\d@$!%*?&]{8,}"),
+    NUMBER("[0-9]{10}"),
+    EMAIL("[a-zA-Z]+\\w+@[a-zA-Z]+\\.(com|net|edu|org)"),
+    CONFIRM("y(es)?\\Z|no?\\Z"),
+    MONETARY("\\d+(\\.\\d\\d)?"),
+    ANY(".*");
 
     public final String pattern;
 
