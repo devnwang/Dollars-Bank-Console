1c7d2984dcc76435ffeae8be17d82a49ae26daec
nikkijwang <nikkijwang@gmail.com>
2021-09-27 14:22:28 -0700

    add transaction stubs for fund transfers

--- src/com/dollarsbank/utility/DataGeneratorStubUtil.java
+++ src/com/dollarsbank/utility/DataGeneratorStubUtil.java
@@ -18,7 +18,25 @@ public class DataGeneratorStubUtil {
         String transaction = String.format("%s of $%.2f in [%s].%n", action, amount, account.getAccountId());
 
         // Account balance after transaction
-        transaction += String.format("Balance - $%.2f as of %s", account.getBalance(), TIMESTAMP.format(ZonedDateTime.now()));
+        transaction += balanceStatement(account);
+
+        return transaction;
+    }
+
+    // Transaction stub for user making the transfer
+    public static final String transferToStub(double amount, Account from, Customer to) {
+        String transaction = String.format("Transfer of $%.2f from [%s] to %s [%s].%n", amount, from.getAccountId(), to.getFullName(), to.getAccount().getAccountId());
+        
+        transaction += balanceStatement(from);
+
+        return transaction;
+    }
+
+    // Transaction stub for the user receiving the transfer
+    public static final String transferFromStub(double amount, Customer from, Account to) {
+        String transaction = String.format("Transfer of $%.2f from %s [%s] to [%s].%n", amount, from.getFullName(), from.getAccount().getAccountId(), to.getAccountId());
+        
+        transaction += balanceStatement(to);
 
         return transaction;
     }
@@ -43,6 +61,10 @@ public class DataGeneratorStubUtil {
 
         // Update the customer's transactions
         customer.setTransactions(transactions);
+    }
+
+    private static final String balanceStatement(Account acct) {
+        return String.format("Balance - $%.2f as of %s", acct.getBalance(), TIMESTAMP.format(ZonedDateTime.now()));
     }
     
 }
